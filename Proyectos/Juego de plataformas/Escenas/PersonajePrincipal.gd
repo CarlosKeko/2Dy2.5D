27352802c80dcea10c5fa64a2b8287f8fff608c6
CarlosKeko <carloskeko80@gmail.com>
2024-05-16 23:03:22 +0200

    Personaje animaciones casi acabadas

--- Proyectos/Juego de plataformas/Escenas/PersonajePrincipal.gd
+++ Proyectos/Juego de plataformas/Escenas/PersonajePrincipal.gd
@@ -1,27 +1,60 @@
 extends CharacterBody2D
 
-
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
+@onready var anim = $AnimatedSprite2D
+const SPEED = 130.0
+const JUMP_VELOCITY = -300.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
+#Variable para saber la ultima direccion que ha mirado el personaje true = derecha, left izquierda
+var direccion = true 
+
 
 func _physics_process(delta):
+	var direction = Input.get_axis("left", "right")
+	
+	if direccion and direction == 0 and is_on_floor():
+		anim.play("Idle")
+		
+	elif !direccion and direction == 0 and is_on_floor():
+		anim.play("Idle left")
+	
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
+		if direction == 0 and direccion:
+			anim.play("Jump")
+			
+		elif direction == 0 and !direccion:
+			anim.play("Jump left")
 
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
+
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
+
+	if direction == -1:
+		direccion = false
 		velocity.x = direction * SPEED
+		if is_on_floor():
+			anim.play("Run left")
+		
+		else:
+			anim.play("Jump left")
+			
+	elif direction == 1:
+		direccion = true
+		velocity.x = direction * SPEED
+		if is_on_floor():
+			anim.play("Run")
+		
+		else:
+			anim.play("Jump")
+		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
